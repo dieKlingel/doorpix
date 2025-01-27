38ee822494e94c1497f977573000e1e5a18ac503
Kai Mayer <kai@mayer.sh>
2025-01-27 17:38:54 +0000

    refactor: use pkg config instead of raw paths

--- core/internal/drivers/appvideo/build.go
+++ core/internal/drivers/appvideo/build.go
@@ -1,14 +1,4 @@
 package appvideo
 
-// #cgo CPPFLAGS: -DPJ_AUTOCONF=1 -O2 -DPJ_IS_BIG_ENDIAN=0 -DPJ_IS_LITTLE_ENDIAN=1
-// #cgo CFLAGS: -I${SRCDIR}/../../../../3rdparty/pjproject/pjsip/include -I${SRCDIR}/../../../../3rdparty/pjproject/pjlib/include -I${SRCDIR}/../../../../3rdparty/pjproject/pjlib-util/include -I${SRCDIR}/../../../../3rdparty/pjproject/pjmedia/include -I${SRCDIR}/../../../../3rdparty/pjproject/pjnath/include
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../3rdparty/pjproject/pjsip/lib -lpjsua2-aarch64-unknown-linux-gnu -lpjsua-aarch64-unknown-linux-gnu -lpjsip-ua-aarch64-unknown-linux-gnu -lpjsip-simple-aarch64-unknown-linux-gnu -lpjsip-aarch64-unknown-linux-gnu
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../3rdparty/pjproject/pjmedia/lib  -lpjmedia-codec-aarch64-unknown-linux-gnu -lpjmedia-videodev-aarch64-unknown-linux-gnu -lpjmedia-audiodev-aarch64-unknown-linux-gnu -lpjsdp-aarch64-unknown-linux-gnu -lpjmedia-aarch64-unknown-linux-gnu
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../3rdparty/pjproject/pjnath/lib -lpjnath-aarch64-unknown-linux-gnu
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../3rdparty/pjproject/pjlib-util/lib -lpjlib-util-aarch64-unknown-linux-gnu
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../3rdparty/pjproject/pjlib/lib -lpj-aarch64-unknown-linux-gnu
-// #cgo LDFLAGS: -L${SRCDIR}/../../../../3rdparty/pjproject/third_party/lib -lresample-aarch64-unknown-linux-gnu -lspeex-aarch64-unknown-linux-gnu -lgsmcodec-aarch64-unknown-linux-gnu -lsrtp-aarch64-unknown-linux-gnu -lilbccodec-aarch64-unknown-linux-gnu -lg7221codec-aarch64-unknown-linux-gnu -lwebrtc-aarch64-unknown-linux-gnu -lyuv-aarch64-unknown-linux-gnu
-// #cgo LDFLAGS: -lssl -lcrypto -lm -lpthread -luuid -lrt -lasound
-// #cgo LDFLAGS: -lSDL2 -lv4l2 -lvpx -lopenh264
-// #cgo LDFLAGS: -lavformat -lavcodec -lavutil -lswscale
+// #cgo pkg-config: --static libpjproject
 import "C"
