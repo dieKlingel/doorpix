0a75c7207b5ec94edbece3262222af19d6f18881
KoiFresh <kai@mayer.sh>
2025-08-22 19:58:57 +0000

    feat: generate latest bindings

--- core/pkg/pjsua2/build.go
+++ core/pkg/pjsua2/build.go
@@ -1,6 +1,6 @@
 package pjsua2
 
-//go:generate swig -c++ -go -cgo -intgosize 64 -outcurrentdir -I../../../3rdparty/pjproject/pjsip/include -I../../../3rdparty/pjproject/pjlib/include ../../../3rdparty/pjproject/pjsip-apps/src/swig/pjsua2.i
+//go:generate swig -c++ -go -cgo -intgosize 64 -outcurrentdir -I/usr/local/include ../../../3rdparty/pjproject/pjsip-apps/src/swig/pjsua2.i
 
 // #cgo pkg-config: --static libpjproject
 import "C"
