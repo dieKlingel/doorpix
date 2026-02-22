baf9cd1dc05c0704a97c5c575f4d5facbdeaaf26
Kai Mayer <kai@mayer.sh>
2026-02-22 20:27:45 +0100

    ci: run flake check
    Signed-off-by: Kai Mayer <kai@mayer.sh>

--- flake.nix
+++ flake.nix
@@ -13,6 +13,51 @@
     nixpkgs,
     systems,
   }: rec {
+    checks = nixpkgs.lib.genAttrs (import systems) (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        name = "doorpix test";
+
+        doCheck = true;
+        src = ./.;
+
+        # build-time deps
+        nativeBuildInputs = with pkgs; [
+          go
+        ];
+
+        # run-time deps
+        buildInputs = with pkgs; [
+          pkg-config
+          pjsip
+          openssl
+          alsa-lib
+          gst_all_1.gstreamer
+          gst_all_1.gst-plugins-base
+          gst_all_1.gst-plugins-good
+          gst_all_1.gst-plugins-bad
+          gst_all_1.gst-plugins-ugly
+          gst_all_1.gst-libav
+          gst_all_1.gst-vaapi
+        ];
+
+        configurePhase = ''
+          export GOCACHE=$TMPDIR/go-cache
+          export GOMODCACHE=$TMPDIR/go-modcache
+          export XDG_CACHE_HOME=$TMPDIR/xgd-cache
+        '';       
+
+        buildPhase = ''
+          go build -o $out/doorpix cmd/doorpix/main.go
+        '';
+
+        checkPhase = ''
+          go test -v ./...
+        '';
+      };
+    });
+
     devShells = nixpkgs.lib.genAttrs (import systems) (system: let
       pkgs = import nixpkgs { inherit system; };
     in {
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-gst/go-gst v1.4.0
 	github.com/gorilla/mux v1.8.1
 	github.com/stretchr/testify v1.11.1
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -15,5 +16,4 @@ require (
 	github.com/mattn/go-pointer v0.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20251219203646-944ab1f22d93 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
